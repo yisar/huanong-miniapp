aec6c2445d1c5bc05ec851b9693333e26c3b7a99
Yisar <1533540012@qq.com>
2025-07-23 21:01:16 +0800

    EMMM

--- pages/note/note.js
+++ pages/note/note.js
@@ -2,7 +2,8 @@ Page({
   data: {
     title: "",
     clickCount: 0,
-    show: false
+    show: false,
+    imgs: []
   },
   onLoad() {
     const that = this
@@ -41,13 +42,56 @@ Page({
     }, 5000)
   },
 
+  uploadSingleImage() {
+    const that = this
+    my.chooseImage({
+      count: 1,
+
+      sourceType: ['album'],
+      success: function (res) {
+        const path = res.apFilePaths[0]
+        that.uploadImage(path)
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+    });
+
+  },
+
+  uploadImage(path) {
+    const that = this
+    my.uploadFile({
+      filePath: path,
+      url: "https://upload.calibur.cn/upload",
+      name: "upload",
+      hideLoading: false,
+      fileType: "image",
+      success(res) {
+        const data = JSON.parse(res.data)
+        console.log(data.path)
+
+        that.setData({
+          imgs: that.data.imgs.concat([data.path])
+        })
+
+      },
+      fail(err) {
+        console.log(err)
+        my.alert(JSON.stringify(err))
+      }
+    })
+  },
+
   bindFormSubmit(e) {
     const text = e.detail.value.textarea
 
-    const user = my.getStorageSync({key:"userstring"}).data
-    const loc=[this.data.lat,this.data.lon].join(',')
+    const user = my.getStorageSync({
+      key: "userstring"
+    }).data
+    const loc = [this.data.lat, this.data.lon].join(',')
 
-    console.log(user,loc,text)
+    console.log(user, loc, text)
 
     my.request({
       url: `https://huanong.calibur.cn/api/photo`,
@@ -55,11 +99,11 @@ Page({
       dataType: "json",
       data: {
         user,
-        photo: "",
-        extra:"",
+        photo: imgs.join(','),
+        extra: "",
         loc,
         text,
-        cat:'产业调查'
+        cat: '产业调查'
       },
       success(res) {
         my.showToast({
@@ -73,12 +117,15 @@ Page({
   },
 
   add() {
-    const { clickCount, show } = this.data;
+    const {
+      clickCount,
+      show
+    } = this.data;
     const newCount = clickCount + 1;
     this.setData({
       clickCount: newCount
     });
-    if (newCount >= 5 &&!show) {
+    if (newCount >= 5 && !show) {
       this.setData({
         show: true
       });
